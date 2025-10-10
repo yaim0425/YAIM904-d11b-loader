39031772685fc856fcb97694f102bd951f64223b
yaim0425 <yaim0425@gmail.com>
2025-10-10 14:32:39 -0500

    Implementación de GMOD.defines.place_result

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -242,7 +242,7 @@ function This_MOD.get_elements()
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     for _, entity in pairs(data.raw[This_MOD.under]) do
-        valide_entity(GMOD.get_item_create(entity, "place_result"), entity)
+        valide_entity(GMOD.get_item_create(entity, GMOD.defines.place_result), entity)
     end
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
