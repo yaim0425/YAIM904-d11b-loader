75830bb59d04bccb6ddc70e88ffb8c8039a71783
yaim0425 <yaim0425@gmail.com>
2025-10-16 20:16:18 -0500

    GMOD.duplicate_subgroup(space.item.subgroup, Item.subgroup

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -31,7 +31,6 @@ function This_MOD.start()
             --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
             --- Crear los elementos
-            This_MOD.create_subgroup(space)
             This_MOD.create_item(space)
             This_MOD.create_entity(space)
             This_MOD.create_recipe(space)
@@ -257,30 +256,6 @@ end
 
 ---------------------------------------------------------------------------
 
-function This_MOD.create_subgroup(space)
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Validación
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    if not space.item then return end
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-
-
-
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-    --- Crear un nuevo subgrupo
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-    local Old = This_MOD.old_subgroup
-    local New = This_MOD.new_subgroup
-    GMOD.duplicate_subgroup(Old, New)
-
-    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-end
-
 function This_MOD.create_item(space)
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     --- Validación
@@ -320,14 +295,26 @@ function This_MOD.create_item(space)
     --- Entidad a crear
     Item.place_result = space.name
 
+    --- Actualizar el subgrupo
+    Item.subgroup = This_MOD.new_subgroup
+
     --- Actualizar el icono
     Item.icons = {
         { icon = This_MOD.icon_graphics.base },
         { icon = This_MOD.icon_graphics.mask, tint = space.color },
     }
 
-    --- Actualizar el subgrupo
-    Item.subgroup = This_MOD.new_subgroup
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+
+
+
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    --- Crear el subgrupo para el objeto
+    --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+    GMOD.duplicate_subgroup(This_MOD.old_subgroup, Item.subgroup)
 
     --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
